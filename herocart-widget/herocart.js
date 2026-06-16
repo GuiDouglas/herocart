13eb5ddbba107da7a19f1974f20802a47695cf6f
GuiDouglas <gui.alvesdouglas@gmail.com>
2026-06-15 20:13:35 -0400

    load cart

--- herocart-widget/herocart.js
+++ herocart-widget/herocart.js
@@ -1,1 +1,18 @@
 console.log("HeroCart Loaded");
+
+(async () => {
+  try {
+    const response =
+      await fetch("/cart.js");
+
+    const cart =
+      await response.json();
+
+    console.log(
+      "HeroCart Cart:",
+      cart
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
