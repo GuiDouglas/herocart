a9df17e7e45a3207689f76827d9bd6ca1fbc0690
GuiDouglas <gui.alvesdouglas@gmail.com>
2026-06-18 17:47:06 -0400

    wit(update): payment icons container

--- herocart-widget/src/payment-methods.js
+++ herocart-widget/src/payment-methods.js
@@ -4,7 +4,7 @@ export function renderPaymentMethods() {
     <div class="hc-payment-methods">
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/applepay.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/applepay.svg"
         alt="Apple Pay"
         class="hc-payment-icon"
       />
@@ -16,37 +16,37 @@ export function renderPaymentMethods() {
       />
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/paypal.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/paypal.svg"
         alt="PayPal"
         class="hc-payment-icon"
       />
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/americanexpress.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/americanexpress.svg"
         alt="American Express"
         class="hc-payment-icon"
       />
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/visa.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/visa.svg"
         alt="Visa"
         class="hc-payment-icon"
       />
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/mastercard.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/mastercard.svg"
         alt="Mastercard"
         class="hc-payment-icon"
       />
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/Shop-Pay-Logo-SVG_001.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/Shop-Pay-Logo-SVG_001.svg"
         alt="Shop Pay"
         class="hc-payment-icon"
       />
 
       <img
-        src="herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/maestro-svgrepo-com.svg"
+        src="https://herocart.gui-alvesdouglas.workers.dev/src/assets/payment-methods/maestro-svgrepo-com.svg"
         alt="Maestro"
         class="hc-payment-icon"
       />
